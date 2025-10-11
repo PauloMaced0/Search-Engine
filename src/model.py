456d170c8918011a1f79899b9fd003b109331da7
PauloMaced0 <paulomacedo@ua.pt>
2025-10-11 19:06:24 +0100

    change num filters, add padding and normalization

--- src/model.py
+++ src/model.py
@@ -2,16 +2,16 @@ import torch
 import torch.nn as nn
 
 class CNNInteractionBasedModel(nn.Module):
-    def __init__(self, vocab_size, pretrained_embeddings=None, embedding_dim=300, num_filters=32, kernel_size=3, dropout=0.3):
+    def __init__(self, vocab_size, pretrained_embeddings=None, embedding_dim=300, num_filters=64, kernel_size=3, dropout=0.3):
         super().__init__()
         # Embedding layer to map token IDs to dense vectors
         if pretrained_embeddings is not None:
-            self.embedding = nn.Embedding.from_pretrained(pretrained_embeddings, freeze=False, padding_idx=0)
+            self.embedding = nn.Embedding.from_pretrained(pretrained_embeddings, freeze=False)
         else:
-            self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+            self.embedding = nn.Embedding(vocab_size, embedding_dim)
 
         # Convolutional layer to process interaction matrix
-        self.conv = nn.Conv2d(1, out_channels=num_filters, kernel_size=(kernel_size, kernel_size))
+        self.conv = nn.Conv2d(1, out_channels=num_filters, kernel_size=kernel_size, padding=1)
 
         # Activation function
         self.activation = nn.ReLU()
@@ -23,9 +23,6 @@ class CNNInteractionBasedModel(nn.Module):
 
         # Fully connected layer for scoring
         self.fc = nn.Linear(num_filters, 1)
-
-        # Sigmoid for probabilities
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, query, document):
         """
@@ -39,6 +36,10 @@ class CNNInteractionBasedModel(nn.Module):
         # Embed 
         query_embed = self.embedding(query)  # (B, Q, E)
         document_embed = self.embedding(document)  # (B, D, E)
+
+        # normalize before matmul
+        query_embed = query_embed / (query_embed.norm(dim=-1, keepdim=True) + 1e-8)
+        document_embed = document_embed / (document_embed.norm(dim=-1, keepdim=True) + 1e-8)
 
         # Compute interaction matrix
         # Interaction matrix shape: (B, Q, D)
